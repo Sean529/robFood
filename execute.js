502a23445cdd4571d958855fd3a13e250d04e21e
AT <atxiesz@gmal.com>
2022-04-12 13:51:03 +0800

    持续优化

--- execute.js
+++ execute.js
@@ -6,28 +6,58 @@ if (auto.service == null) {
     action: "android.settings.ACCESSIBILITY_SETTINGS",
   })
 }
+function ABwaitForActivity(activity, timeout, period) {
+  period = period || 200
+  var time = new Date().getTime()
+  while (global.currentActivity() != activity) {
+    if (timeout && new Date().getTime() - time >= timeout) {
+      return false
+    }
+    sleep(period)
+  }
+  return true
+}
 
+function ABwaitForPackage(packageName, timeout, period) {
+  period = period || 200
+  var time = new Date().getTime()
+  while (global.currentPackage() != packageName) {
+    if (timeout && new Date().getTime() - time >= timeout) {
+      return false
+    }
+    sleep(period)
+  }
+  return true
+}
 function autoStart() {
   auto.waitFor()
   const SHORT_WAIT = 200
   const LONG_WAIT = 2000
   const appName = "叮咚买菜"
   launchApp(appName)
+  toastLog("开始了等2s")
   sleep(LONG_WAIT)
+  toastLog("到了2s")
 
-  const path = "res/大籽-白月光与朱砂痣.mp3"
-  // const path = '/storage/emulated/0/脚本/at-robFood/res/大籽-白月光与朱砂痣.mp3'
+  function textClickBtn(message) {
+    text(message).findOne().click()
+  }
+
+  // const path = "res/大籽-白月光与朱砂痣.mp3"
+  const path = "/storage/emulated/0/脚本/at-robFood/res/大籽-白月光与朱砂痣.mp3"
   let m = media
   function musicPlay() {
-    musicStop()
+    musicStop("开始播放前>停止音乐")
     m.playMusic(path)
     function mySleep(ms) {
       return new Promise((resolve) => setTimeout(resolve, ms))
     }
+    toastLog("开始播放音乐")
     mySleep(m.getMusicDuration())
   }
 
-  function musicStop() {
+  function musicStop(message) {
+    toastLog(message)
     m.stopMusic()
   }
 
@@ -84,7 +114,6 @@ function autoStart() {
     } else if (text("您选择的送达时间已经失效了，请重新选择").exists()) {
       deliveryTimeError()
     } else if (text("选择送达时间")) {
-      musicStop()
       if (text("今天")) {
         const hasNow =
           text("今天").findOnce() &&
@@ -92,11 +121,13 @@ function autoStart() {
           text("今天").findOnce().parent().children() &&
           text("今天").findOnce().parent().children().length !== 1
         if (hasNow) {
+          musicStop("选择送达时间1>停止音乐")
           // 如果有时间选择，则选择时间
           selectTime()
           sleep(1000)
           payment()
         } else {
+          musicStop("选择送达时间2>停止音乐")
           if (id("iv_dialog_select_time_close").findOne()) {
             id("iv_dialog_select_time_close").findOne().click()
             // 已约满就返回购物车
@@ -104,19 +135,21 @@ function autoStart() {
             sleep(SHORT_WAIT)
             shoppingCartCase()
           } else {
+            musicStop("选择送达时间3>停止音乐")
             back()
             sleep(LONG_WAIT)
             shoppingCartCase()
           }
         }
       } else if (hasText("明天")) {
-        alert(222)
         if (textStartsWith("明天").findOnce().parent().children().length) {
+          musicStop("选择送达时间4>停止音乐")
           // 如果有时间选择，则选择时间
           selectTime()
           sleep(1000)
           payment()
         } else {
+          musicStop("选择送达时间5>停止音乐")
           // 已约满就返回购物车
           id("iv_dialog_select_time_close").findOne().click()
           back()
@@ -124,14 +157,14 @@ function autoStart() {
           shoppingCartCase()
         }
       } else {
-        musicStop()
+        musicStop("选择送达时间>继续payment>停止音乐")
         payment()
       }
     } else {
       musicPlay()
       var clear = confirm("回购物车运行脚本")
       if (clear) {
-        musicStop()
+        musicStop("会购物车运行脚本>停止音乐")
       }
     }
   }
@@ -144,6 +177,10 @@ function autoStart() {
       sleep(2000)
       return payCase()
     }
+    if (hasText("继续结算")) {
+      textClickBtn("继续结算")
+      return payment()
+    }
     if (hasText("返回购物车")) {
       back()
       sleep(SHORT_WAIT)
@@ -154,15 +191,15 @@ function autoStart() {
       if (hasText("前方拥堵")) {
         sleep(2000)
       }
-      payCase()
       musicPlay()
+      payCase()
     } else {
       shoppingCartCase()
     }
   }
 
   function deliveryTimeError() {
-    musicStop()
+    musicStop("deliveryTimeError>停止音乐")
     // 送达时间不正确了，重新选择 / 基本遇不到
     text("选择送达时间").findOnce().click()
     // NOTE: 点击送达时间进行时间选择
@@ -182,20 +219,26 @@ function autoStart() {
   // 点击立即支付后遇到的 case
   function payCase() {
     sleep(2000)
+    if (hasText('继续支付')) {
+      textClickBtn('继续支付')
+      sleep(SHORT_WAIT)
+      return payment()
+    }
     if (hasText("重新加载")) {
+      musicStop("重新加载>停止音乐")
       idClickBtn("ll_reload_action").findOne().click()
       sleep(2000)
       return payCase()
     }
     if (text("您选择的送达时间已经失效了，请重新选择").exists()) {
+      musicStop("您选择的送达时间已经失效了，请重新选择>停止音乐")
       deliveryTimeError()
     } else if (hasText("返回购物车")) {
-      musicStop()
+      musicStop("返回购物车>停止音乐")
       text("返回购物车").findOnce().click()
       sleep(SHORT_WAIT)
       shoppingCartCase()
     } else if (text("选择送达时间")) {
-      musicStop()
       if (text("今天")) {
         const hasNow =
           text("今天").findOnce() &&
@@ -203,25 +246,27 @@ function autoStart() {
           text("今天").findOnce().parent().children() &&
           text("今天").findOnce().parent().children().length !== 1
         if (hasNow) {
+          musicStop("选择送达时间11>停止音乐")
           // 如果有时间选择，则选择时间
           selectTime()
           sleep(1000)
           payment()
         } else {
           if (id("iv_dialog_select_time_close").findOne()) {
+            musicStop("选择送达时间22>停止音乐")
             id("iv_dialog_select_time_close").findOne().click()
             // 已约满就返回购物车
             back()
             sleep(SHORT_WAIT)
             shoppingCartCase()
           } else {
+            musicStop("选择送达时间33>停止音乐")
             back()
             sleep(LONG_WAIT)
             shoppingCartCase()
           }
         }
       } else if (hasText("明天")) {
-        alert(222)
         if (textStartsWith("明天").findOnce().parent().children().length) {
           // 如果有时间选择，则选择时间
           selectTime()
@@ -235,9 +280,11 @@ function autoStart() {
           shoppingCartCase()
         }
       } else {
-        musicStop()
+        musicStop("走支付流程>停止音乐")
         payment()
       }
+    } else {
+      musicPlay()
     }
   }
 
@@ -254,7 +301,7 @@ function autoStart() {
 
   // 去确认订单页
   function confirmOrderCase() {
-    musicStop()
+    musicStop("确认订单页流程>停止音乐")
     // 已勾选的按钮会带数字，所以通过按钮文本来判断购物车中的商品是否失效，如果没有勾选，则无法进入确认订单页面。
     const toSettleAccounts_btn = text("去结算").exists()
     if (toSettleAccounts_btn) {
@@ -275,12 +322,18 @@ function autoStart() {
       payment()
     }
   }
+
   if (hasText("去结算")) {
-    waitForPackage("com.yaya.zone", SHORT_WAIT)
+    toastLog("等结算流程")
+    ABwaitForActivity("com.yaya.zone", SHORT_WAIT)
+    toastLog("开始结算流程")
     confirmOrderCase()
   } else {
-    waitForPackage("com.yaya.zone", SHORT_WAIT)
+    toastLog("等确认订单流程")
+    ABwaitForActivity("com.yaya.zone", SHORT_WAIT)
+    toastLog("开始确认订单流程")
     shoppingCartCase()
   }
 }
+
 autoStart()
